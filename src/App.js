983043a90ffb61bfec58eede0475cae75cde117b
SpurlockC <evilskullygaming@gmail.com>
2024-10-05 09:59:53 -0500

    Fix import paths and resolve missing module

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import Post from './post';
+import Post from './Post';
 
 function App() {
   return (
